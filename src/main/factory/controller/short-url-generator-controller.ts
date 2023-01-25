8a8b2f5bf717fcfc482f1d6fa86235736d9913b1
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2023-01-25 17:45:08 -0300

    feat: add makeShortUrlGeneratorController

---
+++ src/main/factory/controller/short-url-generator-controller.ts
@@ -0,0 +1,8 @@
+import { ShortUrlGeneratorController } from 'src/presentation/controller/short-url-generator-controller';
+import { makeShortUrlGeneratorUseCase } from '../use-case/short-url-generator-use-case-factory';
+
+export function makeShortUrlGeneratorController() {
+  return new ShortUrlGeneratorController(
+    makeShortUrlGeneratorUseCase(),
+  );
+}
