d26cd17ff09cf1c62ce186825c6ea6d113adee2f
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2023-01-25 16:02:19 -0300

    feat: add HashAdapter

---
+++ src/infrastructure/adapter/hash-adapter.ts
@@ -0,0 +1,20 @@
+import { randomBytes } from 'node:crypto';
+import { IHashAdapter } from './interface/hash-adapter-interface';
+
+export class HashAdapter implements IHashAdapter {
+  constructor(
+    private readonly bytes: number,
+  ) {}
+
+  async hasher(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      randomBytes(this.bytes, (err, buffer) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(buffer.toString('hex'));
+      });
+    });
+  }
+}
