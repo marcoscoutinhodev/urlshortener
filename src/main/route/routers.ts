9ae2c9ae2d034f355e6a7d426db223e049d4c14e
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2023-01-27 00:51:53 -0300

    feat: add /:hash route

--- src/main/route/routers.ts
+++ src/main/route/routers.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { expressRouteAdapter } from './express-route-adapter';
 import { makeShortUrlGeneratorController } from '../factory/controller/short-url-generator-controller-factory';
+import { makeGetLongUrlController } from '../factory/controller/get-long-url-controller-factory';
 
 export const router = Router();
 
 router.post('/generate', expressRouteAdapter(makeShortUrlGeneratorController()));
+router.get('/:hash', expressRouteAdapter(makeGetLongUrlController()));
